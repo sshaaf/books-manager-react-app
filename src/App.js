e2679c14202ccdeceb9edbc83aade61acc25a7d7
Shaaf, Syed <sshaaf@redhat.com>
2023-01-25 09:38:18 +0100

    add keycloak support

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.less'
 import Book from './components/Screens/Book'
 import CreateNewBook from './components/Screens/CreateNewBook'
@@ -8,29 +8,50 @@ import {
     BrowserRouter as Router,
     Switch, Route,
 } from 'react-router-dom'
+import Keycloak from 'keycloak-js';
 
 // Routing to the different screens
 
 const App = () => {
     const [books, setBooks] = useState([])
-    return (
-        <Router>
-            <Switch>
-                <Route exact path='/books'>
-                    <BookTable books={books} setBooks={setBooks}/>
-                </Route>
-                <Route exact path='/books/create'>
-                    <CreateNewBook books={books} setBooks={setBooks}/>
-                </Route>
-                <Route exact path='/books/edit/:isbn'>
-                    <EditBook />
-                </Route>
-                <Route exact path='/books/:isbn'>
-                    <Book books={books} setBooks={setBooks}/>
-                </Route>
-            </Switch>
-        </Router>
-    )
+    useEffect(()=>{
+        const keycloak = new Keycloak('/keycloak.json');
+        keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+          setKeycloak(keycloak)
+          setAuthenticated(authenticated)
+        })
+      }, [])
+    const [keycloak, setKeycloak] = useState(null)
+    const [authenticated, setAuthenticated] = useState(false)
+    
+    
+    if (keycloak) {
+            if (authenticated) return ( 
+            <Router>
+                <Switch>
+                    <Route exact path='/books'>
+                        <BookTable books={books} setBooks={setBooks}/>
+                    </Route>
+                    <Route exact path='/books/create'>
+                        <CreateNewBook books={books} setBooks={setBooks}/>
+                    </Route>
+                    <Route exact path='/books/edit/:isbn'>
+                        <EditBook />
+                    </Route>
+                    <Route exact path='/books/:isbn'>
+                        <Book books={books} setBooks={setBooks}/>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+        else return (<div className='my-12'>Unable to initiate auth!</div>)
+    }
+    return(
+        <>
+          <div className='my-12'>Keycloak initializing in a moment...</div>
+        </>
+        )
+
 }
 
 export default App;
--- src/services/booksService.js
+++ src/services/booksService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/books'
+const baseUrl = 'http://localhost:8888/books'
 
 // getting all the books from json file
 const getAll = () => {
